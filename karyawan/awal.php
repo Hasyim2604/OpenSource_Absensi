db21f646569df49d410f1162c5dfe15b4421b54b
Nuvuss <113011780+Nuvuss@users.noreply.github.com>
2023-12-20 12:05:24 +0700

    Update awal.php

--- karyawan/awal.php
+++ karyawan/awal.php
@@ -86,7 +86,7 @@ if (isset($_POST['simpan_ket'])) {
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Masukkan Keterangan Anda</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Masukkan Keterangan</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
